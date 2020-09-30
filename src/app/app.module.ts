c41f6db92e36733059e1b9c0603eb47324df2b49
Benedikt Oehlrich <boe@codemanufaktur.com>
2020-09-30 09:12:29 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScenariosComponent } from './scenarios/scenarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScenariosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/models/scenario.ts
@@ -0,0 +1,5 @@
+export interface Scneario {
+    id: number;
+    name: string;
+    
+}
